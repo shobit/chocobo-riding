0ed7ce744f48ad43810af9b4d33dc4fcf2c30623
Menencia <menencia@gmail.com>
2012-04-15 19:01:24 +0400

    Changements de BDD

--- docs/changes.sql
+++ docs/changes.sql
@@ -1,2 +1,14 @@
 ALTER TABLE  `chocobos` CHANGE  `energy`  `hp` INT( 11 ) NOT NULL DEFAULT  '0',
 CHANGE  `spirit`  `mp` INT( 11 ) NOT NULL DEFAULT  '0'
+
+UPDATE chocobos SET nb_rides = nb_rides + nb_compets + nb_trainings
+
+ALTER TABLE  `chocobos` DROP  `nb_trainings` ,
+DROP  `nb_compets` ;
+
+ALTER TABLE  `chocobos` CHANGE  `nb_rides`  `nb_races` INT( 11 ) NOT NULL DEFAULT  '0'
+
+ALTER TABLE  `circuits` ADD  `script` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULLIF
+
+ALTER TABLE  `circuits` DROP  `surface`
+
