f13891d897de8f0bf63a107ff2a6a5d43b2b3f96
Menencia <menencia@gmail.com>
2012-04-23 16:13:43 +0400

    Les chocobos ne peuvent courir que sur des circuits de même classe que le leur

--- application/i18n/fr_FR/race.php
+++ application/i18n/fr_FR/race.php
@@ -34,5 +34,11 @@ $lang = array (
 		'gains' => "Gains",
 		'exp' => "Expérience",
 		'apt' => "Aptitudes"
-	)
+	),
+	'pos1' => '1er',
+	'pos2' => '2nd',
+	'pos3' => '3è',
+	'pos4' => '4è',
+	'pos5' => '5è',
+	'pos6' => '6è',
 );
