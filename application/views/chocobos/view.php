e1b32bd7719798539c36dca753b5a8c1aba3c023
Menencia <menencia@gmail.com>
2012-04-24 22:20:58 +0400

    Ajout des barres de progression dans le profil

--- application/views/chocobos/view.php
+++ application/views/chocobos/view.php
@@ -1,3 +1,43 @@
+<style>
+	div.line_tab {
+		margin-bottom: 20px;
+	}
+	
+	div.line {
+		font-size: 1.0em;
+		padding: 6px 0 0 6px;
+	}
+	
+	div.line1 {
+		font-size: 1.1em;
+		letter-spacing: 1px;
+		font-weight: bold;
+	}
+	
+	div.line_rouge, span.line_rouge {color: #800000;}
+	div.line_vert, span.line_vert {color: #184500;}
+	div.line_bleu, span.line_bleu {color: #191970;}
+	div.line_gris, span.line_gris {color: #555;}
+	div.line_noir, span.line_noir {color: #000;}
+	
+	div.bord_rouge {border-bottom: 1px dotted #800000;}
+	div.bord_vert {border-bottom: 1px dotted #184500;}
+	div.bord_bleu {border-bottom: 1px dotted #191970;}
+	div.bord_gris {border-bottom: 1px dotted #555;}
+	div.bord_noir {border-bottom: 1px dotted #000;}
+	
+	.label {width: 70%; float: left; font-variant: small-caps;}
+	.value {text-align: right;}
+	.p-wrapper {border-top: 1px solid #bbb;}
+	.progress {height: 3px; margin-bottom: 10px;}
+	.p-green {background-color: #090;}
+	.p-yellow {background-color: #900;}
+	.p-red {background-color: #900;}
+	
+	span.attributes {font-weight: normal; font-size: 10px;}
+	
+</style>
+
 <?php
 	//echo html::script('js/fusion.js');
 	//echo html::script('js/chocobo_view.js');
@@ -27,13 +67,48 @@
 ?>
 <div class="leftPart">
 
+	<div class="line">
+		<div class="label">côte</div>
+		<div class="value"><?php echo $chocobo->display_fame() ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display(1 - $chocobo->fame + 0.01, 1, 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">vitesse max</div>
+		<div class="value"><?php echo $chocobo->max_speed ?> km/h</div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->max_speed, 175, 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">prix</div>
+		<div class="value"><?php echo $chocobo->get_price() ?> gils</div>
+		<div class="p-wrapper">
+			<?php echo progress::display(0, 1, 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">naissance</div>
+		<div class="value"><?php $tl = gen::time_left($chocobo->birthday); echo $tl['short']; ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display(0, 1, 199) ?>
+		</div>
+	</div>
+	
+	
+	
+
 	<table class="informations">
 	
 		<?php if (!$mine) { ?>
 		<tr>
 			<td class="label"><?= Kohana::lang('chocobo.user') ?></td>
 			<td class="value">
-			<?= html::anchor('user/view/'.$chocobo->user->username, $chocobo->user->username); ?>
+			<?php echo html::anchor('user/view/'.$chocobo->user->username, $chocobo->user->username) ?>
 			</td>
 		</tr><?php
 		}
@@ -63,60 +138,6 @@
 				</td>
 			</tr>
 		<?php } ?>
-		<tr>
-			<td class="label"><?= Kohana::lang('chocobo.level') ?></td>
-			<td class="value">
-			<?= '<b>'.$chocobo->level.'</b> <small>/'.$chocobo->lvl_limit.'</small>'; ?>
-			</td>
-		</tr>
-		<?php if ($chocobo->user->id == $user->id) { ?>
-		<tr>
-			<td class="label"><?= Kohana::lang('chocobo.experience') ?></td>
-			<td class="value">
-			<?= html::image('images/icons/exp.png').' '.$chocobo->xp.'%' ?>
-			</td>
-		</tr>
-		<?php } ?>
-		<tr>
-			<td class="label"><?= Kohana::lang('chocobo.cel') ?></td>
-			<td class="value">
-			<?php
-				echo html::image('images/theme/etoile.png').' ';
-				echo $chocobo->display_fame();
-			?>
-			</td>
-		</tr>
-		<tr>
-			<td class="label"><?= Kohana::lang('chocobo.max_speed') ?></td>
-			<td class="value">
-			<?php
-				echo $chocobo->max_speed." km/h";
-			?>
-			</td>
-		</tr>
-		<tr>
-			<td class="label"><?= Kohana::lang('chocobo.price') ?></td>
-			<td class="value">
-			<?php
-				
-				echo html::image('images/theme/gil.gif'). ' ';
-				$content = $chocobo->get_price().' <small>gils</small>';
-				echo $content;
-			?>
-			</td>
-		</tr>
-		<tr>
-			<td class="label"><?= Kohana::lang('chocobo.birthday') ?></td>
-			<td class="value">
-			<?php $tl = gen::time_left($chocobo->birthday); echo $tl['short']; ?>
-			</td>
-		</tr>
-		<tr>
-			<td class="label"><?= Kohana::lang('chocobo.status') ?></td>
-			<td class="value">
-			<?= $chocobo->display_status() ?>
-			</td>
-		</tr>
 
 	</table>
 
@@ -124,88 +145,83 @@
 
 <div class="leftPart">
 
+	<div class="line">
+		<div class="label">niveau</div>
+		<div class="value"><?php echo $chocobo->level ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->level, $chocobo->lvl_limit, 199) ?>
+		</div>
+	</div>
+
+	<div class="line">
+		<div class="label">xp</div>
+		<div class="value"><?php echo $chocobo->xp ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->xp, 100, 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">hp</div>
+		<div class="value"><?php echo $chocobo->hp ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->hp, $chocobo->attr('hp_limit'), 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">mp</div>
+		<div class="value"><?php echo $chocobo->mp ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->mp, $chocobo->attr('mp_limit'), 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">rage</div>
+		<div class="value"><?php echo $chocobo->rage ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->rage, $chocobo->attr('rage_limit'), 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">pl</div>
+		<div class="value"><?php echo $chocobo->pl ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->pl, $chocobo->attr('pl_limit'), 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">vitesse</div>
+		<div class="value"><?php echo $chocobo->speed ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->speed, 175, 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">endurance</div>
+		<div class="value"><?php echo $chocobo->endur ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->endur, 175, 199) ?>
+		</div>
+	</div>
+	
+	<div class="line">
+		<div class="label">intelligence</div>
+		<div class="value"><?php echo $chocobo->endur ?></div>
+		<div class="p-wrapper">
+			<?php echo progress::display($chocobo->intel, 175, 199) ?>
+		</div>
+	</div>
+	
 	<?php if ($mine and $chocobo->points >0) { ?>
 	<div id="points">PA à dépenser : 
 		<b><span><?= $chocobo->points ?></span></b></div><br />
 	<?php } ?>
-
-	<!-- VIT END INT -->
-	<?php 
-	$apts = array('speed', 'endur', 'intel');
-	$caracs = array('pl', 'hp', 'mp');
-	$colours = array('rouge', 'vert', 'bleu');
 	
-	for ($i=0; $i<=2; $i++) 
-	{
-		?>
-		<div class="line_tab">
-			
-			<!-- APT -->
-			<div class="line1 line_<?= $colours[$i] ?> bord_<?= $colours[$i] ?>">
-				<div class="label">
-					<?= html::image("images/icons/".$apts[$i].".png").' '.
-						Kohana::lang('chocobo.'.$apts[$i]) ?></div>
-				<div class="value">
-					<?php 
-					$apt = $chocobo->attr($apts[$i]);
-					echo '<span id="'.$apts[$i].'">'.$apt."</span>";
-					if ($mine and $chocobo->points >0)
-					{
-						echo ' <span class="speed_up points"><small>';
-						echo html::anchor(
-							'chocobo/aptitude_up/'.$apts[$i], 
-							html::image('images/theme/plus.jpg')
-						);
-						echo '</small></span>';
-					}
-					?>
-				</div>
-			</div>
-			
-			<!-- APT_limit -->
-			<div class="line line_<?= $colours[$i] ?>">
-				<div class="label"><?= Kohana::lang('chocobo.'.$caracs[$i]) ?></div>
-				<div class="value">
-				<?php
-					echo floor($chocobo->$caracs[$i]).' ';
-					$apt = $chocobo->attr($caracs[$i].'_limit');
-					echo '<small>/'.$apt.'</small>';
-				?>
-				</div>
-			</div>
-		</div>
-		<?php 
-	} 
-	?>
-	
-	<!-- MORAL -->
-	<div class="line_tab">
-		<div class="line1 line_gris bord_gris">
-			<div class="label"><?= html::image("images/icons/moral.png").' '.Kohana::lang('chocobo.moral') ?></div>
-			<div class="value">
-			<?php
-				echo $chocobo->moral;
-				echo '<span class="attributes">%</span>';
-			?>
-			</div>
-		</div>
-	</div>
-	
-	<!-- RAGE -->
-	<div class="line_tab">
-		<?php if ($mine) { ?>
-		<div class="line1 line_gris bord_gris">
-			<div class="label"><?= html::image("images/icons/rage.png").' '.Kohana::lang('chocobo.rage') ?></div>
-			<div class="value">
-			<?php
-				echo $chocobo->rage;
-				echo '<span class="attributes">/'.$chocobo->attr('rage_limit').'</span>';
-			?>
-			</div>
-		</div>
-		<?php } ?>
-	</div>
-
 </div>
 	
 <?php
