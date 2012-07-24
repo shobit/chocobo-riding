02185f752242e78931eab31ad51ed9adfcd3ab56
Menencia <menencia@gmail.com>
2012-07-25 00:05:07 +0400

    Ajout du script sql pour réinitialiser les champs boxes, items et shop

---
+++ docs/sql/changes.sql
@@ -0,0 +1,4 @@
+ALTER TABLE  `users` CHANGE  `boxes`  `boxes` INT( 3 ) UNSIGNED NOT NULL DEFAULT  '0',
+CHANGE  `items`  `items` INT( 3 ) UNSIGNED NOT NULL DEFAULT  '0',
+CHANGE  `shop`  `shop` INT( 3 ) UNSIGNED NOT NULL DEFAULT  '0'
+
