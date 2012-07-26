005b35b169ba00e9ced2110c0786ab6ce6c52a61
menencia <menencia@gmail.com>
2012-07-27 00:38:22 +0400

    Première version du générateur aléatoire d'équipements

--- application/views/pages/test.php
+++ application/views/pages/test.php
@@ -2,13 +2,9 @@
 
 <?php
 
-$res = array('red' => 13, 'yellow' => 2);
+$e = ORM::factory('equipment');
+$e->generate(0, 1, 3);
 
-echo num::rand_pick($res);
-
- //$e = ORM::factory('nut');
- //$e->generate(0, 1, 3);
-
- //echo $e->vignette();
+echo $e->vignette();
 
 ?>
