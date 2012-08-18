9829dd89935136a8bd17b8d868b564df97a3454f
menencia <menencia@gmail.com>
2012-08-18 14:56:47 +0400

    Ajout de content.sql.

---
+++ docs/sql/content.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Sam 18 Août 2012 à 14:56
+-- Version du serveur: 5.5.9
+-- Version de PHP: 5.3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Base de données: `chocobo-riding`
+--
+
+--
+-- Contenu de la table `designs`
+--
+
+INSERT INTO `designs` VALUES
+(1, 'default', 2, 1),
+(2, 'yellow', 33, 1);
+
+--
+-- Contenu de la table `roles`
+--
+
+INSERT INTO `roles` VALUES
+(1, 'admin'),
+(2, 'modo');
+
+--
+-- Contenu de la table `sites`
+--
+
+INSERT INTO `sites` VALUES
+(1, 12, 1283887189, 'forum/topic/134', '0.15', 0);
+
+--
+-- Contenu de la table `titles`
+--
+
+INSERT INTO `titles` VALUES
+(1, 'avatar_upload', 95, 0),
+(9, 'gils_500', 28, 0),
+(10, 'gils_1000', 17, 0),
+(11, 'gils_5000', 5, 0),
+(12, 'gils_10000', 3, 0),
+(13, 'boxes_3', 58, 0),
+(14, 'boxes_5', 14, 0),
+(15, 'boxes_7', 9, 0),
+(16, 'classe_b', 44, 0),
+(17, 'classe_a', 3, 0),
+(18, 'classe_aplus', 1, 0),
+(19, 'classe_s', 0, 0),
+(20, 'classe_splus', 0, 0),
+(21, 'chocobo_red', 3, 0),
+(22, 'chocobo_blue', 0, 0),
+(23, 'chocobo_green', 0, 0),
+(24, 'chocobo_black', 0, 0),
+(25, 'chocobo_silver', 0, 0),
+(26, 'chocobo_white', 0, 0),
+(27, 'chocobo_gold', 0, 0),
+(37, 'level_10', 45, 0),
+(38, 'level_20', 8, 0),
+(39, 'level_30', 3, 0),
+(40, 'level_40', 1, 0),
+(41, 'level_50', 1, 0),
+(42, 'level_60', 0, 0),
+(43, 'level_70', 0, 0),
+(44, 'level_80', 0, 0),
+(45, 'level_90', 0, 0),
+(46, 'level_100', 0, 0),
+(47, 'fame_075', 13, 0),
+(48, 'fame_050', 8, 0),
+(49, 'fame_025', 3, 0),
+(50, 'fame_001', 3, 0),
+(51, 'vitmax_25', 43, 0),
+(52, 'vitmax_50', 3, 0),
+(53, 'vitmax_100', 0, 0),
+(54, 'vitmax_150', 0, 0),
+(55, 'vitmax_175', 0, 0),
+(292, 'items_12', 19, 0),
+(293, 'items_15', 9, 0),
+(294, 'items_20', 4, 0),
+(295, 'birthdays_25', 0, 0),
+(296, 'birthdays_50', 0, 0),
+(297, 'birthdays_75', 0, 0),
+(298, 'birthdays_100', 0, 0);
